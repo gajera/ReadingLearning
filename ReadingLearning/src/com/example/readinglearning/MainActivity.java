b5f7add5d3ed0824f57dd9f05aae5cde59c31a95
Shirish <Shirish@Shirish-THINK.mshome.net>
2013-06-21 22:48:26 -0700

    Added LoginActivity

--- ReadingLearning/src/com/example/readinglearning/MainActivity.java
+++ ReadingLearning/src/com/example/readinglearning/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.readinglearning;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	
+	private Button login;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		login =(Button)findViewById(R.id.button1);
+		login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent StartLogin=new Intent(null, LoginActivity.class);
+                startActivity(StartLogin);
+            }
+        });
+		
 	}
 
 	@Override
